219055598c2d7d110380bb759cf6a2c7a928d99a
Machika <machika111@gmail.com>
2022-11-30 18:25:30 -0800

    add clear cart function

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
@@ -8,6 +8,8 @@ import Checkout from "./Checkout";
 
 const Cart = (props) => {
   const [isCheckout, setIdCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false); // to display success message
   const cartCxt = useContext(CartContext);
 
   const totalAmount = `$${cartCxt.totalAmount.toFixed(2)}`;
@@ -19,22 +21,30 @@ const Cart = (props) => {
   };
 
   const cartItemAddHandler = (item) => {
-    cartCxt.addItem({...item, amount:1});
+    cartCxt.addItem({ ...item, amount: 1 });
   };
 
   const orderHandler = () => {
     setIdCheckout(true);
-  }
+  };
 
-  const submitOrderHandler = (userData) => {
-    fetch("https://food-order-app-88517-default-rtdb.firebaseio.com/orders.json", {
-      method:"POST",
-      body:JSON.stringify({
-        user:userData,
-        orderItems:cartCxt.items
-      })
-    })
-  }
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    // Extra work -> error handling
+    await fetch(
+      "https://food-order-app-88517-default-rtdb.firebaseio.com/orders.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          user: userData,
+          orderItems: cartCxt.items,
+        }),
+      }
+    );
+    setIsSubmitting(false);
+    setDidSubmit(true);
+    cartCxt.clearCart();// submit done -> clear cart
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
@@ -45,7 +55,7 @@ const Cart = (props) => {
           price={item.price}
           amount={item.amount}
           onRemove={cartItemRemoveHandler.bind(null, item.id)}
-          onAdd = {cartItemAddHandler.bind(null, item)}
+          onAdd={cartItemAddHandler.bind(null, item)}
         />
       ))}
     </ul>
@@ -57,19 +67,47 @@ const Cart = (props) => {
       <button className={classes["button--alt"]} onClick={props.onClose}>
         Close
       </button>
-      {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
     </div>
   );
 
-  return (
-    <Modal onClose={props.onClose}>
+  const cartModalContent = (
+    <React.Fragment>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
       {!isCheckout && modalActions}
+    </React.Fragment>
+  );
+
+  // show the message while submitting
+  const isSubmittingModalContent = <p>Sending order data...</p>;
+
+  const didSubmitHandlerContent = (
+    <React.Fragment>
+      <p>Successfully sent the order</p>
+      <div className={classes.actions}>
+      <button className={classes.button} onClick={props.onClose}>
+        Close
+      </button>
+    </div>
+    </React.Fragment>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitHandlerContent}
     </Modal>
   );
 };
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -58,8 +58,11 @@ const cartReducer = (state, action) => {
         items:updatedItems,
         totalAmount:updatedTotalAmount
     }
+  }
 
-
+  if(action.type === "CLEAR") {
+    // if CLEAR -> show empty cart
+    return defaultCartState
 
   }
   return defaultCartState;
@@ -79,11 +82,16 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({type:"CLEAR"})
+  }
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart:clearCartHandler
   };
 
   return (
